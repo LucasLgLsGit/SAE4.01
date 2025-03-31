0c1aee548ef4523b78ac0b4c00684e95aca6d759
Lucas <lucas.langlois1@etu.univ-lehavre.fr>
2025-03-31 12:42:19 +0200

    Fix UtilisateurRepository

--- app/repositories/UtilisateurRepository.php
+++ app/repositories/UtilisateurRepository.php
@@ -10,7 +10,7 @@ class UtilisateurRepository {
 	}
 
 	public function findAll(): array {
-		$stmt = $this->pdo->query('SELECT * FROM "Utilisateur"');
+		$stmt = $this->pdo->query('SELECT * FROM "utilisateur"');
 		$utilisateurs = [];
 		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			$utilisateurs[] = $this->createUserFromRow($row);
@@ -20,34 +20,34 @@ class UtilisateurRepository {
 
 	private function createUserFromRow(array $row): Utilisateur
 	{
-		return new Utilisateur($row['id_user'], $row['prenom'], $row['nom'], $row['mail'], $row['mdp'], $row['permission']);
+		return new Utilisateur($row['id_user'], $row['mail'], $row['mdp'], $row['permission'], $row['nom'], $row['prenom']);
 	}
 
 	public function create(Utilisateur $utilisateur): bool {
-		$stmt = $this->pdo->prepare('INSERT INTO "Utilisateur" (prenom, nom, mail, mdp, permission) VALUES (:prenom, :nom, :mail, :mdp, :permission)');
+		$stmt = $this->pdo->prepare('INSERT INTO "utilisateur" (mail, mdp, permission, nom, prenom) VALUES (:mail, :mdp, :permission, :nom, :prenom)');
 		return $stmt->execute([
-			'prenom' => $utilisateur->getPrenom(),
-			'nom' => $utilisateur->getNom(),
 			'mail' => $utilisateur->getMail(),
 			'mdp' => password_hash($utilisateur->getMdp(), PASSWORD_BCRYPT),
-			'permission' => $utilisateur->getPermission()
+			'permission' => $utilisateur->getPermission(),
+			'nom' => $utilisateur->getNom(),
+			'prenom' => $utilisateur->getPrenom()
 		]);
 	}
 
 	public function update(Utilisateur $utilisateur): bool {
-		$stmt = $this->pdo->prepare('UPDATE "Utilisateur" SET prenom = :newprenom, nom = :newnom, mail = :newmail, mdp = :newmdp, permission = :newpermission WHERE id_user = :id_user');
+		$stmt = $this->pdo->prepare('UPDATE "utilisateur" SET mail = :newmail, mdp = :newmdp, permission = :newpermission, nom = :newnom, prenom = :newprenom WHERE id_user = :id_user');
 		return $stmt->execute([
 			'id_user' => $utilisateur->getId(),
-			'newprenom' => $utilisateur->getPrenom(),
-			'newnom' => $utilisateur->getNom(),
 			'newmail' => $utilisateur->getMail(),
 			'newmdp' => password_hash($utilisateur->getMdp(), PASSWORD_BCRYPT),
-			'newpermission' => $utilisateur->getPermission()
+			'newpermission' => $utilisateur->getPermission(),
+			'newnom' => $utilisateur->getNom(),
+			'newprenom' => $utilisateur->getPrenom()
 		]);
 	}
 
 	public function findById(int $id): ?Utilisateur {
-		$stmt = $this->pdo->prepare('SELECT * FROM "Utilisateur" WHERE id_user = :id_user');
+		$stmt = $this->pdo->prepare('SELECT * FROM "utilisateur" WHERE id_user = :id_user');
 		$stmt->execute(['id_user' => $id]);
 		$utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
 		if ($utilisateur) {
