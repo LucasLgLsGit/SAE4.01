fd79ed35ff48b3f0fdc1b93117459d6b60a0e9b8
Lucas <lucas.langlois1@etu.univ-lehavre.fr>
2025-04-01 10:47:51 +0200

    Page event fonctionnel mais bug nb event afficher

--- app/controllers/EventController.php
+++ app/controllers/EventController.php
@@ -1,11 +1,28 @@
 <?php
 
 require_once './app/core/Controller.php';
+require_once './app/controllers/EvenementController.php';
 
 class EventController extends Controller
 {
 	public function index()
-	{
-		$this->view('events.html.twig');
-	}
+    {
+        $evenementController = new EvenementController();
+        $upcomingEvents = $evenementController->getUpcomingEvents();
+
+        // Regroupement par année côté PHP
+        $eventsByYear = [];
+        foreach ($upcomingEvents as $event) {
+            $year = $event->getDateDebut()->format('Y');
+            if (!isset($eventsByYear[$year])) {
+                $eventsByYear[$year] = [];
+            }
+            $eventsByYear[$year][] = $event;
+        }
+
+        // Trie les années par ordre décroissant
+        krsort($eventsByYear);
+
+        $this->view('events.html.twig', ['eventsByYear' => $eventsByYear]);
+    }
 }
