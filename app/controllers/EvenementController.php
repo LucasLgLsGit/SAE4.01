4cb1583dc8705b2366e9d0a26fcad6a7922aa773
Ethan Lepareur <ethan.lepareur@gmail.com>
2025-04-01 08:19:29 +0200

    Création de Evenement Controller début de actualité Controller

---
+++ app/controllers/EvenementController.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once './app/core/Controller.php';
+require_once './app/services/EvenementService.php';
+require_once './app/trait/FormTrait.php';
+require_once './app/trait/AuthTrait.php';
+
+class EvenementController extends Controller {
+
+	use FormTrait;
+	use AuthTrait;
+
+	public function index()
+	{
+		$repository = new EvenementRepository();
+		$evenements = $repository->findAll();
+
+		$this->view('/event/index.html.twig', ['evenements' => $evenements]);
+	}
+
+	public function create() {
+		$data = $this->getAllPostParams();
+		$errors = [];
+
+		if (!empty($data)) {
+			try {
+				$eventService = new EvenementService();
+				$eventService->create($data);
+				$this->redirectTo('evenements.php');
+			} catch (Exception $e) {
+				$errors = explode(', ', $e->getMessage());
+			}
+		}
+
+		$this->view('/event/create.html.twig', ['errors' => $errors, 'data' => $data]);
+	}
+
+	public function update() {
+		$id = $this->getQueryParam('id_event');
+
+		if ($id === null) {
+			throw new Exception("L'identifiant évenement est requis !");
+		}
+
+		$data = $this->getAllPostParams();
+		$errors = [];
+
+		if (!empty($data)) {
+			try {
+				$eventService = new EvenementService();
+				$eventService->update($id, $data);
+				$this->redirectTo('evenements.php');
+			} catch (Exception $e) {
+				$errors = explode(', ', $e->getMessage());
+			}
+		}
+
+		$this->view('/event/update.html.twig', 'Modification d\'un évenement', ['errors' => $errors, 'data' => $data, 'id_event' => $id]);
+	}
+}
