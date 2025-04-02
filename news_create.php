631f5a351928d87d1a29f8c8c523ce5a079c9526
Ethan Lepareur <ethan.lepareur@gmail.com>
2025-04-02 14:54:14 +0200

    Fix dans les fichiers publics

--- news_create.php
+++ news_create.php
@@ -2,7 +2,6 @@
 require_once './app/controllers/ActualiteController.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	error_log('Requête reçue : ' . print_r($_POST, true));
 	$controller = new ActualiteController();
-	$controller->delete();
+	$controller->create();
 }
--- user_update_permission.php
+++ user_update_permission.php
@@ -4,7 +4,4 @@ require_once './app/controllers/UtilisateurController.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$controller = new UtilisateurController();
 	$controller->updatePermission();
-} else {
-	http_response_code(405);
-	echo json_encode(['success' => false, 'message' => 'Méthode non autorisée.']);
 }
