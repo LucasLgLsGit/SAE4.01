2a839f61e6f51a5d17bb6abfe98e797ff9e3b602
Lucas <lucas.langlois1@etu.univ-lehavre.fr>
2025-04-03 13:31:05 +0200

    fichiers actions "news" mutualisés

--- news_admin.php
+++ news_admin.php
@@ -1,5 +1,29 @@
 <?php
+require_once './app/controllers/ActualiteController.php';
 require_once './app/controllers/NewsAdminController.php';
 
-$controller = new NewsAdminController();
-$controller->index();
+$ActualiteController = new ActualiteController();
+$NewsAdminController = new NewsAdminController();
+
+$action = $_GET['action'] ?? 'index';
+
+switch ($action) {
+    case 'create':
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $ActualiteController->create();
+        }
+        break;
+    case 'update':
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $ActualiteController->update();
+        }
+        break;
+    case 'delete':
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $ActualiteController->delete();
+        }
+        break;
+    default:
+        $NewsAdminController->index();
+        break;
+}
--- news_delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once './app/controllers/ActualiteController.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$controller = new ActualiteController();
-	$controller->delete();
-}
